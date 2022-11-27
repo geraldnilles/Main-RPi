b46f2cc630790e2187d46284c9907a1fe694d7eb
Gerald Nilles <gerald@nilleye.com>
2022-11-27 13:19:13 -0800

    Dont upgrade base packages

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 sudo apt update
-sudo apt upgrade
+#sudo apt upgrade
 
-sudo apt install git build-essential chrpath diffstat liblz4-tool wpasupplicant awscli tmux
+sudo apt install -y git build-essential chrpath diffstat liblz4-tool wpasupplicant awscli tmux
 
 
 

ffdc69900f387b80ad74e0a632ddf877cb3dcc03
Gerald Nilles <gerald@nilleye.com>
2022-11-27 19:56:11 -0800

    update meta layer, delete WICs before upload

--- save.sh
+++ save.sh
@@ -2,6 +2,10 @@
 
 cd "$(dirname "$0")"
 
+# Delete all WIC images to save space 
+# TODO, figure out a better way to set this env file
+rm poky/build/tmp/deploy/images/*/*.wic
+
 tar cJf geraldpi-artifacts.tar.xz poky/build/tmp/deploy/images 
 
 aws s3 cp geraldpi-artifacts.tar.xz s3://gnyoctobuilds

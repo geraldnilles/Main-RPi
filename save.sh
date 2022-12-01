fb96418bf595ca82f784a581af72ee00861de6fc
Gerald Nilles <gerald@nilleye.com>
2022-11-30 19:50:11 -0800

    Add git hash and default password

--- save.sh
+++ save.sh
@@ -6,6 +6,9 @@ cd "$(dirname "$0")"
 # TODO, figure out a better way to set this env file
 rm poky/build/tmp/deploy/images/*/*.wic
 
+# Save the git hash
+git rev-parse HEAD > poky/build/tmp/deploy/images/githash
+
 tar cJf geraldpi-artifacts.tar.xz poky/build/tmp/deploy/images 
 
 aws s3 cp geraldpi-artifacts.tar.xz s3://gnyoctobuilds

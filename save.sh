ad1f17835e7897f95687d728f11d759007977698
Gerald Nilles <gerald@nilleye.com>
2022-12-02 15:54:49 -0800

    Store log files in output package

--- save.sh
+++ save.sh
@@ -9,6 +9,12 @@ rm poky/build/tmp/deploy/images/*/*.wic
 # Save the git hash
 git rev-parse HEAD > poky/build/tmp/deploy/images/githash
 
+# Copy the conole log to the output image
+cp -r poky/build/tmp/log poky/build/tmp/deploy/images/log
+
+# Looks for any errors and copies those logfiles too
+find poky/build/tmp/log -name console-latest.log | xargs -n 1 cat | grep "Logfile of failure" | awk 'BEGIN { FS=":"} { print $3}' | xargs -I{} -n 1 cp {} poky/build/tmp/deploy/images/log/
+
 tar cJf geraldpi-artifacts.tar.xz poky/build/tmp/deploy/images 
 
 aws s3 cp geraldpi-artifacts.tar.xz s3://gnbuildartifacts

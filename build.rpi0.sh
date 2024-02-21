6abc5d28b5b725edce4066154e3c9719fced3344
Gerald Nilles <gerald@nilleye.com>
2024-02-20 21:31:52 -0800

    Bump openembedded and add speaker to build script

--- build.rpi0.sh
+++ build.rpi0.sh
@@ -8,5 +8,5 @@ WIFI_SSID="$WIFI_SSID" WIFI_PASS="$WIFI_PASS" ./config.rpi0.sh
 cd "$(dirname "$0")"
 . poky/oe-init-build-env
 bitbake geraldpi-cec
-#bitbake geraldpi-speaker
+bitbake geraldpi-speaker
 

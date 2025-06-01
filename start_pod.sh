eb1653772fe110edbc3ebf47b08bafe421e56d62
Gerald Nilles <geraldnilles@gmail.com>
2025-06-01 16:20:56 -0700

    Add command to directly build from Container

--- start_pod.sh
+++ start_pod.sh
@@ -1,3 +1,6 @@
 
-podman run -it --userns=keep-id -v /home/gerald/Code/Main-RPi:/home/ubuntu/yocto --dns 8.8.8.8 yocto-builder:latest /bin/bash
+#  Debugging
+# podman run -it --userns=keep-id -v /home/gerald/Code/Main-RPi:/home/ubuntu/yocto --dns 8.8.8.8 yocto-builder:latest /bin/bash
 
+#  Building
+podman run -it --userns=keep-id -v /home/gerald/Code/Main-RPi:/home/ubuntu/yocto --dns 8.8.8.8 yocto-builder:latest /home/ubuntu/yocto/runall.sh "$1" "$2"

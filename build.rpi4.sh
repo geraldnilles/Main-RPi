acba29e098ce96416d1c8e24247bec9ae5dcd719
Gerald Nilles <geraldnilles@gmail.com>
2025-06-04 20:18:08 -0700

    Build documents

--- build.rpi4.sh
+++ build.rpi4.sh
@@ -8,7 +8,7 @@ WIFI_SSID="$WIFI_SSID" WIFI_PASS="$WIFI_PASS" ./config.rpi4.sh
 cd "$(dirname "$0")"
 . poky/oe-init-build-env
 
-#bitbake geraldpi-documents
+bitbake geraldpi-documents
 bitbake geraldpi-webapps
 #bitbake geraldpi-scam
 
